b4395e0bb8bbeed2dbd9e3f6fe783e20863f7c20
atemple <templeandrew@outlook.com>
2019-12-05 12:02:56 -0500

    Add integration tests with Cypress

---
+++ cypress/integration/pages/index.spec.js
@@ -0,0 +1,18 @@
+
+describe('Home page', () => {
+  /*
+   * Visits the page before each test
+   */
+  beforeEach(() => {
+    cy.log(`Visiting http://localhost:3000`);
+    cy.visit('/');
+  });
+
+  it('should have a home link', () => {
+    cy.get('a[href="/"]').should('have.length', 1);
+  });
+
+  it('should have a title', () => {
+    cy.get('h1.title').should('have.length', 1);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,17 @@ module.exports = {
   ],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
-    '<rootDir>/node_modules/'
+    '<rootDir>/node_modules/',
+    '<rootDir>/cypress/'
   ],
-  moduleNameMapper: {
+  moduleNameMapper: { // https://alexjover.com/blog/enhance-jest-configuration-with-module-aliases/
     "@components(.*)$": "<rootDir>/components/$1",
     "@pages(.*)$": "<rootDir>/pages/$1",
     "@public(.*)$": "<rootDir>/public/$1"
   },
   coverageThreshold: {
     global: {
-      statements: -10, // can have a maximum of 10 un-covered statements
+      statements: -10, // maximum 10 un-covered statements
       branches: 80,
       functions: 80,
       lines: 80
