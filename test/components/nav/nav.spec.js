4aff476317808918c584dad97ab30c8d40ed9caf
atemple <templeandrew@outlook.com>
2019-12-09 09:11:46 -0500

    Fix unit tests

--- test/components/nav/nav.spec.js
+++ test/components/nav/nav.spec.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallowWithIntl, mountWithIntl } from '@lib/i18n';
 
 import { Nav } from '@components/nav';
 
 describe('Nav', () => {
+
   it('should render without throwing an error', () => {
-    const component = shallow(<Nav />);
+    const component = shallowWithIntl(<Nav />);
+    expect(component).toBeDefined();
+  });
+
+  it('should have a home link', () => {
+    const component = mountWithIntl(<Nav />);
     expect(component.find('a').first().text()).toBe('Home');
   });
 });
---
+++ test/lib/i18n/locale-utilities.spec.js
@@ -0,0 +1,72 @@
+import { getLocale, getServerLocale, getBrowserLocale } from '@lib/i18n';
+
+describe('Locale utilities', () => {
+
+  it('should get browser locale default', () => {
+    Object.defineProperty(navigator, 'languages', { get: () => null, configurable: true });
+    Object.defineProperty(navigator, 'language', { get: () => null, configurable: true });
+    const result = getBrowserLocale();
+    expect(result).toBe('en');
+  });
+
+  it('should get browser locale with provided default', () => {
+    Object.defineProperty(navigator, 'languages', { get: () => null, configurable: true });
+    Object.defineProperty(navigator, 'language', { get: () => null, configurable: true });
+    const result = getBrowserLocale('fr');
+    expect(result).toBe('fr');
+  });
+
+  it('should get browser locale from navigator language', () => {
+    Object.defineProperty(navigator, 'languages', { get: () => null, configurable: true });
+    Object.defineProperty(navigator, 'language', { get: () => 'de', configurable: true });
+    const result = getBrowserLocale();
+    expect(result).toBe('de');
+  });
+
+  it('should get browser locale from the first navigator language', () => {
+    Object.defineProperty(navigator, 'languages', { get: () => ['es', 'fr'], configurable: true });
+    Object.defineProperty(navigator, 'language', { get: () => null, configurable: true });
+    const result = getBrowserLocale();
+    expect(result).toBe('es');
+  });
+
+  it('should get server locale default', () => {
+    const result = getServerLocale();
+    expect(result).toBe('en');
+  });
+
+  it('should get server locale with provided default', () => {
+    const result = getServerLocale(null, 'fr');
+    expect(result).toBe('fr');
+  });
+
+  it('should get server locale from the request', () => {
+    const req = {
+      headers: {
+        'accept-language': 'de;q=0.9'
+      }
+    };
+    const result = getServerLocale(req);
+    expect(result).toBe('de');
+  });
+
+  it('should get locale in the browser', () => {
+    // not sure how to spy on functions that aren't attached to a class/object -- have to just run it
+    Object.defineProperty(navigator, 'languages', { get: () => null, configurable: true });
+    Object.defineProperty(navigator, 'language', { get: () => 'it', configurable: true });
+    const result = getLocale(null);
+    expect(result).toBe('it');
+  });
+
+  it('should get locale on the server', () => {
+    // not sure how to spy on functions that aren't attached to a class/object -- have to just run it
+    const req = {
+      headers: {
+        'accept-language': 'es;q=0.9'
+      }
+    };
+    const result = getLocale(req);
+    expect(result).toBe('es');
+  });
+});
+
