19ea0a72cbcd75aa0938b44e156f91ac0e1e972c
atemple <templeandrew@outlook.com>
2020-02-04 13:11:49 -0500

    Remove example actions

--- redux/actions.js
+++ redux/actions.js
@@ -1,4 +1,3 @@
 export * from './app/app-actions';
 export * from './auth/auth-actions';
-export * from './example/example-actions';
 
--- redux/example/example-actions.js
+++
@@ -1,8 +0,0 @@
-
-export const exampleActions = {
-  increment: () => ({ type: 'INCREMENT' }),
-  decrement: () => ({ type: 'DECREMENT' }),
-  failure: (error) => ({ type: 'EXAMPLE_FAILURE', error: (error || true) }),
-  exampleSaga: () => ({ type: 'EXAMPLE_SAGA' })
-};
-
