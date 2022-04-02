c7f3759391df24485fcb6700102081e4dde89038
atemple <templeandrew@outlook.com>
2022-04-02 16:40:58 -0400

    Modal wondering

--- src/features/app/appSlice.spec.js
+++ src/features/app/appSlice.spec.js
@@ -4,10 +4,29 @@ import { createSelector } from 'reselect';
 const initialState = {
   loadingCount: 0,
   modalOpen: false,
-  modalTitle: '',
-  modalContent: null,
   error: false
 };
+
+// need to be able to navigate from Redux(?)
+// need to be able to create modals and stuff from Redux(?)
+
+// on modals, could have a thunk(?) with something like this https://github.com/GA-MO/react-confirm-alert/blob/master/src/index.js#L170-L175
+// with the modal component passed as content:
+//
+// React.render(<Modal>{action.payload.ModalContent}</Modal>, modalTarget);
+//
+// then modals could be written as components rather than a title+content arguments to a function,
+// but you could still call it like openModal(MyModalCompenent, modalProps)
+//
+// does it make sense to have that connected to Redux in any way?
+// doesn't really seem like it, but not sure where else you're supposed to put that kind of thing -- common/utils/modal?
+// would possibly violate "reducers should not have side effects" -- does this apply to thunks? https://redux.js.org/style-guide/style-guide#reducers-must-not-have-side-effects
+//
+// what could be sweet would be to have the modal function return a promise somehow, with the resolve/reject functions passed to the modal component as props
+// then you could probably use the modal in a thunk and get a result from it
+//
+// Maybe that's this thing https://github.com/alexn400/async-modals
+// but then how would you open the modal in a thunk?
 
 export const appSlice = createSlice({
   name: 'app',
@@ -18,17 +37,12 @@ export const appSlice = createSlice({
     },
     showModal: (state, action) => {
       state.modalOpen = true;
-      state.modalTitle = action.payload.modalTitle;
-      state.modalContent = action.payload.modalContent;
     },
     hideModal: (state) => {
       state.modalOpen = false;
-      state.modalTitle = initialState.modalTitle;
-      state.modalContent = initialState.modalContent;
     },
     setAppLoading: (state, action) => {
-      const loadingCount = Math.max(0, state.loadingCount + (action.payload ? 1 : -1));
-      state.loadingCount = loadingCount;
+      state.loadingCount = Math.max(0, state.loadingCount + (action.payload ? 1 : -1));
     },
     clearState: (state, action) => {
       return initialState;
