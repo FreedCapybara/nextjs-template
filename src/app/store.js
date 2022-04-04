a15c038585cb924b5625df1d55657f1e46fe3dcd
atemple <templeandrew@outlook.com>
2022-04-04 15:38:52 -0400

    Fix the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,25 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { rootReducer } from '@app/root-reducer';
+import appReducer from '@features/app/appSlice';
 
 export function createStore() {
-  const reducer = rootReducer;
-  const middleware = [];
-
-  if (process.env.NODE_ENV !== 'production') {
-    // add Redux devtools
-    const { composeWithDevTools } = require('redux-devtools-extension');
-    middleware.push(composeWithDevTools);
-
-    // add redux-immutable-state-invariant
-    // https://redux.js.org/style-guide/style-guide#do-not-mutate-state
-    const reduxImmutableStateInvariant = require('redux-immutable-state-invariant').default();
-    middleware.push(reduxImmutableStateInvariant);
-  }
-
   return configureStore({
-    reducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware)
+    reducer: {
+      app: appReducer
+    }
   });
 }
 
