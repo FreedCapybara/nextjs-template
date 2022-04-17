7ccdc28b8e891ed4f1c5163f67cde7f0cc1fd839
atemple <templeandrew@outlook.com>
2022-04-17 15:21:43 -0400

    Log errors

--- src/features/app/appSlice.js
+++ src/features/app/appSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 const initialState = {
   loadingCount: 0
 };
@@ -12,6 +14,10 @@ function pendingActionMatcher(action) {
 
 function completedActionMatcher(action) {
   return action.type.endsWith('/fulfilled') || action.type.endsWith('/rejected');
+}
+
+function rejectedActionMatcher(action) {
+  return action.type.endsWith('/rejected');
 }
 
 export const appSlice = createSlice({
@@ -26,6 +32,13 @@ export const appSlice = createSlice({
       .addMatcher(completedActionMatcher, (state, action) => {
         // decrement loading when a request finishes
         state.loadingCount = Math.max(0, state.loadingCount - 1);
+      })
+      .addMatcher(rejectedActionMatcher, (state, action) => {
+        if (dev) {
+          // this is a side effect and therefore not in line with the style guide,
+          // but otherwise runtime errors that occur in thunks tend to be silent
+          console.error(action.error);
+        }
       });
   }
 });
