9885c27d053f25d03c73d7f11fe44817524b4827
atemple <templeandrew@outlook.com>
2022-05-02 17:14:31 -0400

    Add a dummy auth API endpoint

---
+++ src/pages/api/sandbox/auth-test.js
@@ -0,0 +1,15 @@
+import { getSession } from 'next-auth/react'
+
+export default async (req, res) => {
+  const session = await getSession({ req });
+
+  if (session) {
+    res.send({
+      content: "This is protected content. You can access this content because you are signed in."
+    });
+  } else {
+    res.send({
+      content: "You must be sign in to view the protected content on this page."
+    });
+  }
+}
