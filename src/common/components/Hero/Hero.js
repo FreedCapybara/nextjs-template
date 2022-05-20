9646b285a579296559823b29e84a1cdbc1adbd4b
atemple <templeandrew@outlook.com>
2022-05-20 13:59:03 -0400

    Get started on the landing page

--- src/common/components/Hero/Hero.js
+++ src/common/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.module.scss';
 
 export function Hero(props) {
   return (
-    <div>
+    <div className={styles.hero}>
       {props.children}
     </div>
   );
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -3,12 +3,14 @@ import styles from './Landing.module.scss'
 import { LandingPageLayout } from '@components/LandingPageLayout';
 import { LandingPageSection } from '@components/LandingPageSection';
 import { Hero } from '@components/Hero';
+import { Emoji } from '@components/Emoji';
 
 export function Landing() {
   return (
     <LandingPageLayout title="The ultimate Next.js boilerplate">
       <Hero>
-        <h1>Landing</h1>
+        <h1>The ultimate Next.js boilerplate <Emoji>&#x1f680;</Emoji></h1>
+        <h3>A fully featured app template to kick off your next project.</h3>
       </Hero>
 
       <LandingPageSection>
