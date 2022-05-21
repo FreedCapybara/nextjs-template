44a63c2ce240391caa91669e131ff87fd5a2c64c
atemple <templeandrew@outlook.com>
2022-05-21 10:31:49 -0400

    Extract the toolbar to a component

--- src/common/components/LandingNav/LandingNav.js
+++ src/common/components/LandingNav/LandingNav.js
@@ -1,53 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
-import { signIn } from 'next-auth/react';
 import styles from './LandingNav.module.scss';
 
-import theme from '@app/theme';
-
-import { FiExternalLink } from 'react-icons/fi';
-import { Toolbar } from '@components/Toolbar';
-import { ToolbarGroup } from '@components/ToolbarGroup';
-import { TabBar } from '@components/TabBar';
-import { Logo } from '@components/Logo';
+import { MainToolbar } from '@components/MainToolbar';
 
 export function LandingNav(props) {
   return (
     <div className={styles.navWrapper}>
-      <Toolbar>
-        <ToolbarGroup>
-          <Link href="/">
-            <a>
-              <Logo />
-            </a>
-          </Link>
-          <Link href="/">
-            <a className={styles.appTitle}>
-              {theme.appName}
-            </a>
-          </Link>
-        </ToolbarGroup>
-
-        <ToolbarGroup>
-          <a
-            className="text-button"
-            href="https://github.com/FreedCapybara/nextjs-template"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub <FiExternalLink />
-          </a>
-          <button className="text-button" onClick={signIn}>
-            Sign in
-          </button>
-          <Link href="/signup">
-            <a className="button">
-              Sign up
-            </a>
-          </Link>
-        </ToolbarGroup>
-      </Toolbar>
+      <MainToolbar />
     </div>
   );
 }
---
+++ src/common/components/MainToolbar/MainToolbar.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import styles from './MainToolbar.module.scss';
+
+import theme from '@app/theme';
+
+import { FiExternalLink } from 'react-icons/fi';
+import { Toolbar } from '@components/Toolbar';
+import { ToolbarGroup } from '@components/ToolbarGroup';
+import { Logo } from '@components/Logo';
+import { AvatarMenu } from '@components/AvatarMenu';
+
+export function MainToolbar(props) {
+  const { data: session } = useSession();
+
+  return (
+    <Toolbar>
+      <ToolbarGroup>
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.appTitle}>
+            {theme.appName}
+          </a>
+        </Link>
+      </ToolbarGroup>
+
+      <ToolbarGroup>
+        <Link href="/demo/components">
+          <a className="text-button">
+            Components
+          </a>
+        </Link>
+
+        <Link href="/demo/pages">
+          <a className="text-button">
+            Pages
+          </a>
+        </Link>
+
+        <a
+          className="text-button"
+          href="https://github.com/FreedCapybara/nextjs-template"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub <FiExternalLink />
+        </a>
+
+        {session ? (
+          <>
+          <AvatarMenu email={session.user.email} align="right">
+            <Link href="/account">
+              <a>Account</a>
+            </Link>
+            <button onClick={signOut}>
+              Logout
+            </button>
+          </AvatarMenu>
+          </>
+        ) : (
+          <>
+          <button className="text-button" onClick={signIn}>
+            Sign in
+          </button>
+          <Link href="/signup">
+            <a className="button">
+              Sign up
+            </a>
+          </Link>
+          </>
+        )}
+      </ToolbarGroup>
+    </Toolbar>
+  );
+}
+
+MainToolbar.propTypes = {
+};
+
