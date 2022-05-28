7c7701d11e423ba515a1724865824c39dbf5a438
atemple <templeandrew@outlook.com>
2022-05-28 12:50:59 -0400

    More component demos

--- src/pages/demo/components/ComponentsDemo.js
+++ src/pages/demo/components/ComponentsDemo.js
@@ -12,6 +12,7 @@ import { FileDrop } from '@components/FileDrop';
 import { FilePicker } from '@components/FilePicker';
 import { Form } from '@components/Form';
 import { FormField } from '@components/FormField';
+import { LoadingSpinner } from '@components/LoadingSpinner';
 import { Logo } from '@components/Logo';
 import { Modal } from '@components/Modal';
 import { NavMenu } from '@components/NavMenu';
@@ -31,16 +32,20 @@ export function ComponentsDemo(props) {
     >
       <div className={styles.wrapper}>
         <div className="container">
-          <h1>Components</h1>
+          <div className={styles.titleWrapper}>
+            <Logo />
+            <h1>Components</h1>
+          </div>
           <p>
             A demo of the built-in UI elements.
             <br />
-            Since they're just included in the source code for this app,
-            you can change them to your liking!
+            They're all included in the source code for this app,
+            so you can easily change them to your liking!
           </p>
 
+          {/* Avatar */}
           <div className={styles.componentSection}>
-            <h3>Avatar</h3>
+            <h3>&lt;Avatar /&gt;</h3>
             <p>
               Shows the <a href="https://en.gravatar.com/" target="_blank" rel="noopener noreferrer">Gravatar image</a> for an email address!
               You can modify request parameters, such as the default image, in gravatar-utils.js (under common/utils/gravatar/).
@@ -52,10 +57,11 @@ export function ComponentsDemo(props) {
             />
           </div>
 
+          {/* AvatarMenu */}
           <div className={styles.componentSection}>
-            <h3>AvatarMenu</h3>
+            <h3>&lt;AvatarMenu /&gt;</h3>
             <p>
-              Creates a menu from the avatar. Great for logged-in users!
+              Creates a menu from the avatar. Great for logged-in users! (Try clicking it)
             </p>
             <AvatarMenu
               email="test@spacegiraffe.io"
@@ -91,76 +97,125 @@ export function ComponentsDemo(props) {
             </AvatarMenu>
           </div>
 
+          {/* DeleteButton */}
           <div className={styles.componentSection}>
-            <h3>DeleteButton</h3>
+            <h3>&lt;DeleteButton /&gt;</h3>
             <p>
               A button that requires multiple clicks before firing its action.
             </p>
             <DeleteButton onDelete={() => alert('Deleted!')} />
           </div>
 
+          {/* Emoji */}
           <div className={styles.componentSection}>
-            <h3>Emoji</h3>
+            <h3>&lt;Emoji /&gt;</h3>
+            <p>
+              Normalizes emoji text styles, ensuring they render sharply and in color.
+            </p>
+            <div className={styles.largeTextWrapper}>
+              <Emoji>&#x1f680;</Emoji>
+              <Emoji>&#x1f44d;</Emoji>
+              <Emoji>&#x1f389;</Emoji>
+              <Emoji>&#x1f525;</Emoji>
+              <Emoji>&#x1f953;</Emoji>
+              <Emoji>&#x1f363;</Emoji>
+              <Emoji>&#x1f408;</Emoji>
+            </div>
           </div>
 
+          {/* EmptyState */}
           <div className={styles.componentSection}>
-            <h3>EmptyState</h3>
+            <h3>&lt;EmptyState /&gt;</h3>
+            <p>
+              Show this when you don't have any data to display.
+              The image is configurable in app/theme.js!
+            </p>
+            <div className={styles.flexStartWrapper}>
+              <EmptyState>
+                No records found.
+              </EmptyState>
+            </div>
           </div>
 
+          {/* ErrorMessage */}
           <div className={styles.componentSection}>
-            <h3>ErrorMessage</h3>
+            <h3>&lt;ErrorMessage /&gt;</h3>
+            <p>Styles error messages consistently.</p>
+            <div className={styles.flexStartWrapper}>
+              <ErrorMessage>
+                <Emoji>&#x1f480;&#x1f525;</Emoji> CRITICAL_FAILURE <Emoji>&#x1f525;&#x1f480;</Emoji>
+              </ErrorMessage>
+            </div>
           </div>
 
+          {/* FileDrop */}
           <div className={styles.componentSection}>
-            <h3>FileDrop</h3>
+            <h3>&lt;FileDrop /&gt;</h3>
           </div>
 
+          {/* FilePicker */}
           <div className={styles.componentSection}>
-            <h3>FilePicker</h3>
+            <h3>&lt;FilePicker /&gt;</h3>
           </div>
 
+          {/* Form */}
           <div className={styles.componentSection}>
-            <h3>Form</h3>
+            <h3>&lt;Form /&gt;</h3>
           </div>
 
+          {/* FormField */}
           <div className={styles.componentSection}>
-            <h3>FormField</h3>
+            <h3>&lt;FormField /&gt;</h3>
           </div>
 
+          {/* LoadingSpinner */}
           <div className={styles.componentSection}>
-            <h3>Logo</h3>
+            <h3>&lt;LoadingSpinner /&gt;</h3>
           </div>
 
+          {/* Logo */}
           <div className={styles.componentSection}>
-            <h3>Modal</h3>
+            <h3>&lt;Logo /&gt;</h3>
           </div>
 
+          {/* Modal */}
           <div className={styles.componentSection}>
-            <h3>NavMenu</h3>
+            <h3>&lt;Modal /&gt;</h3>
           </div>
 
+          {/* NavMenu */}
           <div className={styles.componentSection}>
-            <h3>ProgressBar</h3>
+            <h3>&lt;NavMenu /&gt;</h3>
           </div>
 
+          {/* ProgressBar */}
           <div className={styles.componentSection}>
-            <h3>Swatch</h3>
+            <h3>&lt;ProgressBar /&gt;</h3>
           </div>
 
+          {/* Swatch */}
           <div className={styles.componentSection}>
-            <h3>Tab</h3>
+            <h3>&lt;Swatch /&gt;</h3>
           </div>
 
+          {/* Tab */}
           <div className={styles.componentSection}>
-            <h3>TabBar</h3>
+            <h3>&lt;Tab /&gt;</h3>
           </div>
 
+          {/* TabBar */}
           <div className={styles.componentSection}>
-            <h3>ToggleSwitch</h3>
+            <h3>&lt;TabBar /&gt;</h3>
           </div>
 
+          {/* ToggleSwitch */}
           <div className={styles.componentSection}>
-            <h3>ValidatedInput</h3>
+            <h3>&lt;ToggleSwitch /&gt;</h3>
+          </div>
+
+          {/* ValidatedInput */}
+          <div className={styles.componentSection}>
+            <h3>&lt;ValidatedInput /&gt;</h3>
           </div>
         </div>
       </div>
