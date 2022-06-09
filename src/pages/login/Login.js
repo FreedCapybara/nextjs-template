c59d648761ba11beaa593aeb4fc2e62d4725bd4d
atemple <templeandrew@outlook.com>
2022-06-08 21:32:19 -0400

    Improve the OAuth login buttons

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,10 +3,15 @@ import styles from './Login.module.scss';
 
 import { nextjsUtils } from '@utils/nextjs';
 
+import { GoMarkGithub } from 'react-icons/go';
 import { TwoPaneLayout } from '@components/TwoPaneLayout';
 
 export function Login(props) {
   const { providers } = props;
+
+  const iconMap = {
+    'github': <GoMarkGithub />
+  };
 
   return (
     <TwoPaneLayout
@@ -16,8 +21,13 @@ export function Login(props) {
     >
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
+          <button
+            className="cta-button"
+            onClick={() => signIn(provider.id)}
+          >
+            <span className={styles.providerName}>
+              Sign in with {provider.name}&nbsp;{iconMap[provider.id]}
+            </span>
           </button>
         </div>
       ))}
